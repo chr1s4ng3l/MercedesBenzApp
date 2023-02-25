7840b38feb2840ab16f253e92c7f8adc23c0ccbf
Consultant <christopher.tamayocazarez@gmail.com>
2023-02-24 21:28:50 -0500

    Added Cleaning Home Fragment

--- app/src/main/java/com/example/mercedesbenzapp/view/HomeFragment.kt
+++ app/src/main/java/com/example/mercedesbenzapp/view/HomeFragment.kt
@@ -1,23 +1,13 @@
 package com.example.mercedesbenzapp.view
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.app.Dialog
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Location
-import android.media.audiofx.Equalizer.Settings
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.green
-import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -30,7 +20,7 @@ import com.example.mercedesbenzapp.utils.UIState
 import com.example.mercedesbenzapp.utils.ViewType
 import com.example.mercedesbenzapp.viewmodel.MercedesViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -42,10 +32,12 @@ import com.google.android.gms.maps.model.MarkerOptions
 private const val TAG = "HomeFragment"
 
 class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
-    GoogleMap.OnMyLocationClickListener, GoogleMap.OnMapClickListener {
+    GoogleMap.OnMapClickListener {
 
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var map: GoogleMap
-    private val coordinates = LatLng(33.9091, -84.4792)
+    var mLat = 33.9091
+    var mLon = -84.4792
 
 
     companion object {
@@ -64,8 +56,8 @@ class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
         MercedesAdapter {
             mercedesViewModel.name = it.name
             mercedesViewModel.id = it.id
+            mercedesViewModel.price = it.location.address1.toString()
             mercedesViewModel.image = it.image
-            mercedesViewModel.price = it.price
             mercedesViewModel.rating = it.rating
             mercedesViewModel.distance = it.distance
             mercedesViewModel.phone = it.phone
@@ -78,6 +70,8 @@ class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
 
     }
 
@@ -99,7 +93,6 @@ class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
 
         createFragment()
 
-        //mercedesViewModel.getAllBusinessByLoc(coordinates.latitude, coordinates.longitude)
         getAllBusiness()
         progressBar()
 
@@ -124,7 +117,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
                 }
                 is UIState.SUCCESS<List<RestaurantDomain>> -> {
 
-                    it.response.forEach{
+                    it.response.forEach {
                         listVT.add(ViewType.RESTAURANT(it))
                     }
 
@@ -150,76 +143,56 @@ class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
     }
 
 
-    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         map.setOnMyLocationButtonClickListener(this)
         map.setOnMapClickListener(this)
-        map.setOnMyLocationClickListener(this)
         enableLocation()
-        val marker = MarkerOptions().position(coordinates).title("Me")
-        map.clear()
-        map.addMarker(marker)
 
 
     }
 
 
-    //Location permission
-    fun isLocationPermissionGranted() = ContextCompat
-        .checkSelfPermission(
-            requireContext(),
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-
-
-    @SuppressLint("MissingPermission")
     private fun enableLocation() {
         if (!::map.isInitialized) return
-        //Permissions Granted? Yes
-        if (isLocationPermissionGranted()) {
 
-            map.isMyLocationEnabled = false
-            map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 10f), 2000, null)
-            mercedesViewModel.getAllBusinessByLoc(coordinates.latitude, coordinates.longitude)
+        requestLocation()
 
 
-
-        }
-        //No permissions granted? No
-        else {
-            requestLocation()
-        }
     }
 
-    @SuppressLint("MissingPermission")
     private fun requestLocation() {
+        val task = fusedLocationProviderClient.lastLocation
         mercedesViewModel.isLoading.postValue(false)
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
+        if (ActivityCompat.checkSelfPermission(
                 requireActivity(),
                 Manifest.permission.ACCESS_FINE_LOCATION
-            )
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
         ) {
-            android.app.AlertDialog.Builder(requireContext())
-                .setMessage("To continue, turn on device location, which uses Google's locations service.")
-                .setPositiveButton("GO") { Dialog, _ ->
-                    startActivity(Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS))
-                }
-                .setNegativeButton("NO THANKS") { Dialog, _ ->
-                    map.isMyLocationEnabled = false
-
-                }
-                .create().show()
-        } else {
+            map.isMyLocationEnabled = false
             ActivityCompat.requestPermissions(
                 requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                 REQUEST_LOCATION
             )
+            return
+        } else {
+            map.isMyLocationEnabled = true
+        }
+
+        task.addOnSuccessListener {
+            it?.let {
+                val coordinates = LatLng(it.latitude, it.longitude)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 15f), 2000, null)
+                mercedesViewModel.getAllBusinessByLoc(it.latitude, it.longitude)
+
+            }
         }
 
     }
 
-    @SuppressLint("MissingPermission")
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -228,71 +201,39 @@ class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButto
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             REQUEST_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                map.isMyLocationEnabled = true
-
-            } else {
-                android.app.AlertDialog.Builder(requireContext())
-                    .setMessage("To continue, turn on device location, which uses Google's locations service.")
-                    .setPositiveButton("GO") { Dialog, _ ->
-                        startActivity(Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS))
-                    }
-                    .setNegativeButton("NO THANKS") { Dialog, _ ->
-                        map.isMyLocationEnabled = false
-
-                    }
-                    .create().show()
+                map.isMyLocationEnabled = ActivityCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireActivity(),
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
 
             }
-
             else -> {}
 
         }
     }
 
-    @SuppressLint("MissingPermission")
     override fun onResume() {
         super.onResume()
-        if (!isLocationPermissionGranted()) {
-            if (!::map.isInitialized) return
-            map.isMyLocationEnabled = false
+        if (!::map.isInitialized) return
+        requestLocation()
 
-            android.app.AlertDialog.Builder(requireContext())
-                .setMessage("To continue, turn on device location, which uses Google's locations service.")
-                .setPositiveButton("GO") { Dialog, _ ->
-                    startActivity(Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS))
-                }
-                .setNegativeButton("NO THANKS") { Dialog, _ ->
-                    map.isMyLocationEnabled = false
-
-                }
-                .create().show()
-
-        }
     }
 
     override fun onMyLocationButtonClick(): Boolean {
         return false
     }
 
-    override fun onMyLocationClick(p0: Location) {
-        val coordinates = LatLng(p0.latitude, p0.longitude)
-        val marker = MarkerOptions().position(coordinates).title("Me")
-        map.clear()
-        map.addMarker(marker)
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 20f), 2000, null)
-
-        mercedesViewModel.getAllBusinessByLoc(p0.latitude, p0.longitude)
-
-    }
 
     override fun onMapClick(p0: LatLng) {
 
         val coordinates = LatLng(p0.latitude, p0.longitude)
-        val marker = MarkerOptions().position(coordinates).title("Me")
+        val marker = MarkerOptions().position(coordinates).title("Restaurants near here")
         map.clear()
         map.addMarker(marker)
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 10f), 2000, null)
-
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 12f), 2000, null)
         mercedesViewModel.getAllBusinessByLoc(p0.latitude, p0.longitude)
 
     }
